9b5bf2c0294fc1df292f632c308e3e5873f88486
Taylor Bisset <taylorbisset@gmail.com>
2023-11-16 13:01:25 -0800

    main.cpp header added

---
+++ CS132-Program6-TemplateClass/main.cpp
@@ -0,0 +1,25 @@
+// main.cpp
+
+/*
+ Name:          Taylor Bisset
+ Section:       A
+ Program Name:  CS132-Program4-LinkedList
+
+ Description:   This program modifies my previous Linked List project 
+                into a template class. It should still keep the original purpose: 
+                Manages sorted doubly linked lists of strings.
+                Reads data from input files, manipulates lists,
+                and writes results to output files.
+
+Repo:           https://github.com/TaylorBisset/CS132-Program6-TemplateClass 
+                https://github.com/TaylorBisset/CS132-Program4-LinkedList 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
