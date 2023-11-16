36293b358b4c137ab4a345b618fbe6a3b611d7e0
Taylor Bisset <taylorbisset@gmail.com>
2023-11-16 13:07:51 -0800

    node.h added

---
+++ CS132-Program6-TemplateClass/node.h
@@ -0,0 +1,29 @@
+// node.h
+
+/*
+Declares the Node class, which is used to create nodes in a linked list.
+It defines the structure and interface of the Node class.
+*/
+
+#ifndef NODE_H
+#define NODE_H
+
+#include "TBString.h"
+
+class Node
+{
+public:
+    Node()
+        :
+        next(nullptr), prev(nullptr) {}
+
+    Node(const TBString& data)
+        :
+        data(data), next(nullptr), prev(nullptr) {}
+
+    TBString data;
+    Node* next;
+    Node* prev;
+};
+
+#endif // !NODE_H
